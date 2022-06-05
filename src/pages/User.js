e97b02362efe1a4902d8370023e7a23016196df7
DennisL10 <fl0368032019@unab.edu.sv>
2022-06-05 00:10:12 -0600

    ...

--- src/pages/User.js
+++ src/pages/User.js
@@ -1,10 +1,26 @@
 import '../styles/App.css';
 import { Component } from 'react';
+import logo from '../assets/logo.png';
+import Input from '../components/Atoms/Input';
+import Button from '../components/Atoms/buttons/button';
 
 class User extends Component {
   render() {
-    return;
-  }
+    return(
+      <div className="App">
+        <header className="App-header">
+          <div className="wrapper fadeInDown">
+            <div id="formContent">
+              <div className="fadeIn first">
+                <img src={logo} width="400px" alt="User Icon" />
+              </div>
+            </div>
+          </div>
+        </header>
+      </div>
+
+
+    );}
 }
 
 export default User;
